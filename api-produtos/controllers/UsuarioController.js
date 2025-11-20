2d7573cbae9f10c62a6c877609b01e27fa21ea94
Siss-a <sissaklarosk@gmail.com>
2025-11-20 14:50:24 -0300

    tentativa com o catalogo e crud dos produtos/lari

--- api-produtos/controllers/UsuarioController.js
+++ api-produtos/controllers/UsuarioController.js
@@ -341,3 +341,4 @@ class UsuarioController{
     }
     
 }
+export default UsuarioController
--- api-produtos/routes/authRotas.js
+++ api-produtos/routes/authRotas.js
@@ -1,6 +1,8 @@
+// routes/authRotas.js
 import express from 'express';
 import AuthController from '../controllers/AuthController.js';
-import { authMiddleware, adminMiddleware } from '../middlewares/authMiddleware.js';
+import UsuarioController from '../controllers/UsuarioController.js';
+import { authMiddleware } from '../middlewares/authMiddleware.js';
 
 const router = express.Router();
 
@@ -11,35 +13,26 @@ router.post('/registrar', AuthController.registrar);
 // Rotas protegidas (precisam de autenticação)
 router.get('/perfil', authMiddleware, UsuarioController.obterPerfil);
 
-// fetch("http://localhost:3000/api/usuarios", {
-//     method: "GET",
-//     headers: {
-//         Bearer: token
-//     }
-// })
-
 // Rotas OPTIONS para CORS (preflight requests)
 router.options('/login', (req, res) => {
-    res.header('Access-Control-Allow-Origin', '*');
-    res.header('Access-Control-Allow-Methods', 'POST, OPTIONS');
-    res.header('Access-Control-Allow-Headers', 'Content-Type');
-    res.sendStatus(200);
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Methods', 'POST, OPTIONS');
+  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  res.sendStatus(200);
 });
 
 router.options('/registrar', (req, res) => {
-    res.header('Access-Control-Allow-Origin', '*');
-    res.header('Access-Control-Allow-Methods', 'POST, OPTIONS');
-    res.header('Access-Control-Allow-Headers', 'Content-Type');
-    res.sendStatus(200);
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Methods', 'POST, OPTIONS');
+  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  res.sendStatus(200);
 });
 
 router.options('/perfil', (req, res) => {
-    res.header('Access-Control-Allow-Origin', '*');
-    res.header('Access-Control-Allow-Methods', 'GET, OPTIONS');
-    res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-    res.sendStatus(200);
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Methods', 'GET, OPTIONS');
+  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  res.sendStatus(200);
 });
 
 export default router;
-
-
