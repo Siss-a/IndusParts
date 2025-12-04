3cb56bce86e0d4bcf700b31aa1505067f452a027
gabrielhub123 <gabriel.pllegleg@gmail.com>
2025-12-04 14:05:59 -0300

    bcd coisas

--- api-produtos/migrations/20251203_insert_table_produtos.sql
+++ api-produtos/migrations/20251203_insert_table_produtos.sql
@@ -10,7 +10,7 @@ VALUES
 INSERT INTO produtos 
 (nome, preco, descricao, img, fornecedor, categoria, estoque)
 VALUES
-('Pinça ER', 40.50, 'Componente para fixar ferramentas em máquinas, garantindo precisão e segurança.', 'pincaER.png', 'MetalMax', 'Acessórios de Fixação', 15);
+('Pinça ER', 40.50, 'Componente para fixar ferramentas em máquinas, garantindo precisão e segurança.', 'pincaER.png', 'MetalMax', 'Fixação', 15);
 
 INSERT INTO produtos 
 (nome, preco, descricao, img, fornecedor, categoria, estoque)
@@ -20,17 +20,17 @@ VALUES
 INSERT INTO produtos 
 (nome, preco, descricao, img, fornecedor, categoria, estoque)
 VALUES
-('CoroCut® 2', 210, 'Ferramenta projetada para operações de corte, canal e perfilamento com alta precisão.', 'past.png', 'Cortes', 'Ferramentas de Corte e Canal', 210);
+('CoroCut® 2', 210, 'Ferramenta projetada para operações de corte, canal e perfilamento com alta precisão.', 'past.png', 'Cortes', 'Cortes', 210);
 
 INSERT INTO produtos 
 (nome, preco, descricao, img, fornecedor, categoria, estoque)
 VALUES
-('CoroMill 345', 180, 'Fresa de faceamento de alto desempenho, ideal para remoção de material eficiente.', 'pfres.png', 'MetalMax', 'Fresas de Usinagem', 210);
+('CoroMill 345', 180, 'Fresa de faceamento de alto desempenho, ideal para remoção de material eficiente.', 'pfres.png', 'MetalMax', 'Usinagem', 210);
 
 INSERT INTO produtos 
 (nome, preco, descricao, img, fornecedor, categoria, estoque)
 VALUES
-('Haste Cilíndrica', 420, 'Haste cilíndrica compatível com o sistema CoroChuck 970, utilizada para alta precisão na fixação de ferramentas.', 'haste.png', 'Sandvik Coromant', 'Acessórios de Fixação', 10);
+('Haste Cilíndrica', 420, 'Haste cilíndrica compatível com o sistema CoroChuck 970, utilizada para alta precisão na fixação de ferramentas.', 'haste.png', 'Sandvik Coromant', 'Fixação', 10);
 
 INSERT INTO produtos 
 (nome, preco, descricao, img, fornecedor, categoria, estoque)
